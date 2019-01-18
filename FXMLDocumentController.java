87a47dc1adfe2436e520672298e78d07491542e8
rodolfo <45057543+Srodo95@users.noreply.github.com>
2019-01-18 02:48:37 -0300

    Update FXMLDocumentController.java

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -77,23 +77,21 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void copiarTexto(ActionEvent event) throws IOException{
         File archivoDestino = new File(tf_archivoDestino.getText());
-        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(archivoDestino))) {
-            File archivoPrincipal = new File(tf_archivoPrincipal.getText());
-            try (BufferedReader lector = new BufferedReader(new FileReader(archivoPrincipal))) {
-                String linea;
-                boolean eof = false;
-                while(!eof){
-                    linea = lector.readLine();
-                    if(linea != null){
-                        escritor.write(linea);
-                    }else{
-                        eof = true;
-                    }
-                }
-                lector.close();
-                escritor.close();
+        BufferedWriter escritor = new BufferedWriter(new FileWriter(archivoDestino));
+        File archivoPrincipal = new File(tf_archivoPrincipal.getText());
+        BufferedReader lector = new BufferedReader(new FileReader(archivoPrincipal));
+        String linea;
+        boolean eof = false;
+        while(!eof){
+            linea = lector.readLine();
+            if(linea != null){
+                escritor.write(linea);
+            }else{
+                eof = true;
             }
         }
+        lector.close();
+        escritor.close();
     }
     
     @FXML
